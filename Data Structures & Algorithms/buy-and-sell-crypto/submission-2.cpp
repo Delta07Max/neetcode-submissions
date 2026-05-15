8daf1b78ef50ac3cfd20643a0621e53a08580998
Delta07Max <chitodiyalakshit@gmail.com>
2026-05-15 19:59:38 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int n = prices.size();
+        int buy=prices[0], maxprofit=0;
+        for(int i=1; i<n; i++)
+        {
+            if(prices[i] < buy)
+            {
+                buy = prices[i];
+            }
+            else
+            {
+                maxprofit = max(maxprofit, prices[i]-buy);
+            }
+        }
+        return maxprofit;
+    }
+};
